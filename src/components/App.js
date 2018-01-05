489d55ad6d735ca2ea81227867c35f60d3cd9bb5
Hebert Porto <hebertporto@gmail.com>
2018-01-05 20:55:15 -0200

    filizando primeira rodada de testes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
 
 class App extends Component {
+  state = {
+    gitfs: []
+  }
+
+  addGift = () => {
+    const { gitfs } = this.state
+    const newId = gitfs.length + 1
+    gitfs.push({ id: newId })
+    this.setState({ gitfs })
+  }
+
   render() {
     return (
       <div>
         <h2>Gift Giver</h2>
+        <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
       </div>
     )
   }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -7,3 +7,12 @@ const app = shallow(<App />)
 it('render correctly', () => {
   expect(app).toMatchSnapshot()
 });
+
+it('initialize state with an empty list of gifts', () => {
+  expect(app.state().gitfs).toEqual([])
+})
+
+it('adds a new gift to state when clicking the add gifit is clicked', () => {
+  app.find('.btn-add').simulate('click')
+  expect(app.state().gitfs).toEqual([{id: 1}])
+})
