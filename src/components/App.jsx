9d6f6c11cf62138c134910e79cc8131a8dadeedf
Hebert Porto <hebertporto@gmail.com>
2018-01-06 11:21:58 -0200

    novos testes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,15 @@ class App extends Component {
     return (
       <div>
         <h2>Gift Giver</h2>
+        <div className="gift-list">
+          {
+            this.state.gitfs.map(gift => {
+              return (
+                <div key={gift.id} />
+              )
+            })
+          }
+        </div>
         <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
       </div>
     )
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,17 +2,42 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from './App'
 
-const app = shallow(<App />)
 
-it('render correctly', () => {
-  expect(app).toMatchSnapshot()
-});
+describe ('App', () => {
+  const app = shallow(<App />)
 
-it('initialize state with an empty list of gifts', () => {
-  expect(app.state().gitfs).toEqual([])
+  it('render correctly', () => {
+    expect(app).toMatchSnapshot()
+  });
+
+  it('initialize state with an empty list of gifts', () => {
+    expect(app.state().gitfs).toEqual([])
+  })
+
+ describe ('', () => {
+  beforeEach(() => {
+    app.find('.btn-add').simulate('click')
+  })
+
+  afterEach(() => {
+    app.setState({ gitfs: [] })
+  })
+
+  it('adds a new gift to state when clicking the add gifit is clicked', () => {
+     expect(app.state().gitfs).toEqual([{id: 1}])
+   })
+
+   it ('add new gift on a rendered list of gifts', () => {
+     expect(app.find('.gift-list').children().length).toEqual(1)
+   })
+ })
+
 })
 
-it('adds a new gift to state when clicking the add gifit is clicked', () => {
-  app.find('.btn-add').simulate('click')
-  expect(app.state().gitfs).toEqual([{id: 1}])
-})
+// dit  etq  | det
+
+// describe ('', () => {
+//   it('', () => {
+
+//   });
+// });
